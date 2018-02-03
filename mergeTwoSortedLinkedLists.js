a9acdbf316589e387925a75e65efb1f31777895b
Patrick Miner <patrickminer318@gmail.com>
2018-02-02 19:58:23 -0800

    Optimized for cleaner better code

--- mergeTwoSortedLinkedLists.js
+++ mergeTwoSortedLinkedLists.js
@@ -26,38 +26,66 @@ function ListNode(val) {
 }
 
 const mergeTwoLists = (l1, l2) => {
-  let values = getValues(l1).concat(getValues(l2)).sort((a, b) => {
-    return a - b;
-  });
 
-  if (!values.length) {
-    return [];
+  const mergeHead = { next: null };
+  let crt = mergeHead;
+
+  // while there are any nodes left in either l1 or l2
+  while (l1 || l2) {
+    // if l1 is smaller than l2 OR there is no l2
+    if (!l2 || l1.val < l2.val ) {
+      // set crt.next = l1
+      crt.next = l1;
+      // set crt = crt.next
+      crt = crt.next;
+      // set l1 = l1.next
+      l1 = l1.next;
+      // else if they're equal OR l2 is smaller than l1 OR there is no l1
+    } else {
+      // set crt.next = l2
+      crt.next = l2;
+      // set crt = crt.next
+      crt = crt.next;
+      // set l2 = l2.next
+      l2 = l2.next;
+    }
   }
-
-  let l3 = new ListNode(values.shift());
-
-  for (var i = 0; i < values.length; i++) {
-    getLastNode(l3).next = new ListNode(values[i]);
-  }
-
-  return l3;
-};
-
-const getLastNode = (list) => {
-  while (list.next) {
-    list = list.next
-  }
-  return list;
+  return mergeHead.next;
 }
 
-const getValues = (list) => {
-  const values = [];
-  while (list) {
-    values.push(list.val);
-    list = list.next;
-  }
-  return values;
-}
+// const mergeTwoLists = (l1, l2) => {
+//   let values = getValues(l1).concat(getValues(l2)).sort((a, b) => {
+//     return a - b;
+//   });
+
+//   if (!values.length) {
+//     return [];
+//   }
+
+//   let l3 = new ListNode(values.shift());
+
+//   for (var i = 0; i < values.length; i++) {
+//     getLastNode(l3).next = new ListNode(values[i]);
+//   }
+
+//   return l3;
+// };
+
+// const getLastNode = (list) => {
+//   while (list.next) {
+//     list = list.next
+//   }
+//   return list;
+// }
+
+// const getValues = (list) => {
+//   const values = [];
+//   while (list) {
+//     values.push(list.val);
+//     list = list.next;
+//   }
+//   return values;
+// }
 
 // ---------------
 
