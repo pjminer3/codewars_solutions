4fff1dbfe39e6e1620b9c0fd109340eeac4c1a24
Patrick Miner <patrickminer318@gmail.com>
2018-02-05 23:08:38 -0800

    Refactored solution with test cases

--- combinatorics_rockPaperScissors.js
+++ combinatorics_rockPaperScissors.js
@@ -11,8 +11,8 @@ const rockPaperScissors = (rounds) => {
   }
 
   for (let i = 0; i < noOfCombinations; i++) {
-    for (let j = 0; j < noOfCombinations; j++) {
-      results[i][j] = magicFormula(i, j, rounds);
+    for (let j = 0; j < rounds; j++) {
+      results[i][j] = CHOICES[magicFormula(i, j)];
     }
   }
 
@@ -20,8 +20,33 @@ const rockPaperScissors = (rounds) => {
 }
 
 const magicFormula = (row, col, rounds) => {
-  return Math.floor(row / (CHOICES.length ** (rounds - 1 - col))) % CHOICES.length;
+  return Math.floor(row / (CHOICES.length ** col)) % CHOICES.length;
 }
 
 console.log(rockPaperScissors(3)[0]);
 console.log(rockPaperScissors(3)[1]); 
+console.log(rockPaperScissors(3)[2]); 
+console.log(rockPaperScissors(3)[3]); 
+console.log(rockPaperScissors(3)[4]);
+console.log(rockPaperScissors(3)[5]);
+console.log(rockPaperScissors(3)[6]);
+console.log(rockPaperScissors(3)[7]);
+console.log(rockPaperScissors(3)[8]);
+console.log(rockPaperScissors(3)[9]);
+console.log(rockPaperScissors(3)[10]);
+console.log(rockPaperScissors(3)[11]);
+console.log(rockPaperScissors(3)[12]);
+console.log(rockPaperScissors(3)[13]);
+console.log(rockPaperScissors(3)[14]);
+console.log(rockPaperScissors(3)[15]);
+console.log(rockPaperScissors(3)[16]);
+console.log(rockPaperScissors(3)[17]);
+console.log(rockPaperScissors(3)[18]);
+console.log(rockPaperScissors(3)[19]);
+console.log(rockPaperScissors(3)[20]);
+console.log(rockPaperScissors(3)[21]);
+console.log(rockPaperScissors(3)[22]);
+console.log(rockPaperScissors(3)[23]);
+console.log(rockPaperScissors(3)[24]);
+console.log(rockPaperScissors(3)[25]);
+console.log(rockPaperScissors(3)[26]);
