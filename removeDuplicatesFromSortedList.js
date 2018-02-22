5e113dae82bedc2fcbfa9c8b5d04f9f21a2d104d
Patrick Miner <patrickminer318@gmail.com>
2018-02-21 20:43:27 -0800

    Optimized to best possible solution

--- removeDuplicatesFromSortedList.js
+++ removeDuplicatesFromSortedList.js
@@ -25,44 +25,15 @@ function ListNode(val) {
 };
 
 const deleteDuplicates = function(head) {
-  // handle edge case
-  if (!head) {
-    return [];
-  }  
+  let c = head;
 
-  // set current node
-  let curNode = head;
-  // set next node
-  let curNext = head.next;
-  // set current value
-  let curVal = curNode.val;
-
-
-
-  // while there is a next node
-  while (!!curNext) {
-    // first if statement to handle edge case of curNext being a duplicate and skipping over it
-    if (head.next === curNext && head.val === curNext.val) {
-      head.next = curNext.next;
-      curNext = curNext.next;
-      // if next node's value equals current value
-    } else if (curNext.val === curVal) {
-      // next node = next.next
-      curNext = curNext.next;
+  while (c !== null && c.next !== null) {
+    if (c.val === c.next.val) {
+      c.next = c.next.next;
     } else {
-      // current node.next = next
-      curNode.next = curNext;
-      // current node = next
-      curNode = curNext;
-      // currnt val = current node.val
-      curVal = curNext.val;
-      // next = next.next
-      curNext = curNext.next;
+      c = c.next
     }
   }
-
-  // In case our curNode is pointing to the last node in the list which is a duplicate
-  curNode.next = null;
 
   return head;
 };
