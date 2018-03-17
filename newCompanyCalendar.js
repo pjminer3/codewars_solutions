4bf10ed5f3eabd30f533fd1b40632c61f14915de
Patrick Miner <patrickminer318@gmail.com>
2018-03-16 17:46:08 -0700

    Added a little optimization for better reading and lower space requirements

--- newCompanyCalendar.js
+++ newCompanyCalendar.js
@@ -3,30 +3,27 @@ const mergeRanges = (arr = []) => {
     return a.startTime > b.startTime ? 1 : -1;
   });
   
-  let newTimeObj = arr[0];
-  const newCalendar = [];
+  const newCalendar = [arr[0]];
 
   for (let i = 1; i < arr.length; i++) {
     let current = arr[i];
-    if (current.startTime > newTimeObj.endTime) {
-      // shovel old meeting block in and create a new object to represent a time gap
-      newCalendar.push(newTimeObj);
-      newTimeObj = arr[i];
-    } else if (current.endTime > newTimeObj.endTime) {
-      // re-adjust end time to the furthest
-      newTimeObj.endTime = current.endTime;
+    let lastMeeting = newCalendar[newCalendar.length - 1];
+    // if there is no overlap between current start time and lastMeeting endtime
+    if (current.startTime > lastMeeting.endTime) {
+      // shovel in a new last-meeting
+      newCalendar.push(current);
     }
+    lastMeeting.endTime = Math.max(current.endTime, lastMeeting.endTime);
   }
-  // get the final object in the newCalendar
-  newCalendar.push(newTimeObj);
   return newCalendar;
 };
 
-console.log(mergeRanges([
-  {startTime: 3,  endTime: 5},
-  {startTime: 0,  endTime: 1},
-  {startTime: 10, endTime: 12},
-  {startTime: 4,  endTime: 8},
-  {startTime: 9,  endTime: 10},
-])[2]); //   [ {startTime: 0, endTime: 1}, {startTime: 3, endTime: 8}, {startTime: 9, endTime: 12}, ]
-// console.log(mergeRanges()); // 
+console.log(mergeRanges([{startTime: 3,  endTime: 5}, {startTime: 0,  endTime: 1}, {startTime: 10, endTime: 12}, {startTime: 4,  endTime: 8}, {startTime: 9,  endTime: 10}])[2]); //   [ {startTime: 0, endTime: 1}, {startTime: 3, endTime: 8}, {startTime: 9, endTime: 12}, ]
+console.log(mergeRanges([{startTime: 1, endTime: 2}, {startTime: 2, endTime: 3}])[0]); // { startTime: 1, endTime: 3}
+console.log(mergeRanges(  [{startTime: 1, endTime: 5}, {startTime: 2, endTime: 3}])[0]); // { 1, 5}
+console.log(mergeRanges(  [
+  {startTime: 1, endTime: 10},
+  {startTime: 2, endTime: 6},
+  {startTime: 3, endTime: 5},
+  {startTime: 7, endTime: 9},
+])[0]); // { 1, 10}
