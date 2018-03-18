f4ba7e26fa63f787c223234c628d72ce2b529e0b
Patrick Miner <patrickminer318@gmail.com>
2018-03-18 01:13:00 -0700

    Memoized the function

--- coins.js
+++ coins.js
@@ -1,21 +1,46 @@
-function changePossibilitiesTopDown(amount, coins, index = 0) {
+function changePossibilitiesTopDown(amount, coins) {
   // base cases
-  if (amount === 0) return 1;
-  if (amount < 0) return 0;
-  if (index === coins.length) return 0;
+  const memo = {};
+  function internalCheck (amount, coins, index = 0){
 
-  const currentCoin = coins[index];
-  let posibilities = 0;
+    if (amount === 0) {
+      // memo[`(${amount}, ${index})`] = 1;
+      return 1;
+    }
+    if (amount < 0) {
+      // memo[`(${amount}, ${index})`] = 0;
+      return 0;
+    }
+    if (index === coins.length) {
+      // memo[`(${amount}, ${index})`] = 0;
+      return 0;
+    }
 
-  while (amount >= 0) {
-    posibilities += changePossibilitiesTopDown(amount, coins, index + 1);
-    amount -= currentCoin;
+    console.log(`Trying to make ${amount} with ${coins.slice(index)}`);
+
+    const currentCoin = coins[index];
+    let posibilities = 0;
+
+    while (amount >= 0) {
+      // check to see if its in memo
+      if (!memo.hasOwnProperty(`(${amount}, ${index})`)) {
+        memo[`(${amount}, ${index})`] = internalCheck(amount, coins, index + 1);
+      }
+
+      posibilities += memo[`(${amount}, ${index})`];
+      amount -= currentCoin;
+    }
+
+
+    // console.log(memo);
+    return posibilities;
   }
 
-  return posibilities;
+
+  return internalCheck(amount, coins)
 }
 
 
 
 // console.log(changePossibilitiesTopDown(2, [1, 2])); // 2
-console.log(changePossibilitiesTopDown(4, [1, 2, 3])); // 4
+console.log(changePossibilitiesTopDown(14, [1, 2, 3])); // 4
