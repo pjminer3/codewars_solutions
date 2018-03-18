eb4804c26b88a50000f3640e9a45566a968f91cc
Patrick Miner <patrickminer318@gmail.com>
2018-03-18 00:45:25 -0700

    Optimized solution... next step - memoize

--- coins.js
+++ coins.js
@@ -1,5 +1,21 @@
-const coinCombos = (amount, coinArr) => {
-  // 
-};
+function changePossibilitiesTopDown(amount, coins, index = 0) {
+  // base cases
+  if (amount === 0) return 1;
+  if (amount < 0) return 0;
+  if (index === coins.length) return 0;
 
-console.log(coinCombos(4, [1, 2, 3])); // 4
+  const currentCoin = coins[index];
+  let posibilities = 0;
+
+  while (amount >= 0) {
+    posibilities += changePossibilitiesTopDown(amount, coins, index + 1);
+    amount -= currentCoin;
+  }
+
+  return posibilities;
+}
+
+
+
+// console.log(changePossibilitiesTopDown(2, [1, 2])); // 2
+console.log(changePossibilitiesTopDown(4, [1, 2, 3])); // 4
