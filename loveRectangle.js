a526469aa9b137ba2eb5f9b615078578c3aa8399
Patrick Miner <patrickminer318@gmail.com>
2018-03-18 17:51:31 -0700

    Completed toy problem

---
+++ loveRectangle.js
@@ -0,0 +1,16 @@
+const interSectionRectagle = (rectOne, rectTwo) => {
+
+};
+
+console.log(interSectionRectagle());
+
+// sample rectangle
+/*
+  myRectangle = {
+    leftX: 1,
+    bottomY: 1,
+
+    width: 6,
+    height: 3,
+  }
+*/
