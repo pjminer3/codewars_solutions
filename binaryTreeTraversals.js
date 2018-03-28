bdeb25d61d2a099ff6228d283a10aeb35a4236a9
Patrick Miner <patrickminer318@gmail.com>
2018-03-27 23:06:10 -0700

    Optimized solution

--- binaryTreeTraversals.js
+++ binaryTreeTraversals.js
@@ -4,12 +4,12 @@ function BinaryTreeNode(value) {
   this.left = null;
 }
 
-BinaryTreeNode.prototype.insertLeft = (value) => {
+BinaryTreeNode.prototype.insertLeft = function(value) {
   this.left = new BinaryTreeNode(value);
   return this.left;
 }
 
-BinaryTreeNode.prototype.insertRight = (value) => {
+BinaryTreeNode.prototype.insertRight = function(value) {
   this.right = new BinaryTreeNode(value);
   return this.right;
 }
@@ -22,14 +22,33 @@ tree.insertRight(6);
 tree.right.insertLeft(5);
 tree.right.insertRight(7);
 
+// Called in order because the root is acted on 'in between' it's two children
 const inOrderTraversal = (node) => {
+  if (!node) return;
 
+  inOrderTraversal(node.left);
+  console.log('In-Order Traversal: ', node.value);
+  inOrderTraversal(node.right);
 };
 
+// Callsed 'post' traversal because the root is acted on after (post) its two children
 const postOrderTraversal = (node) => {
+  if (!node) return;
 
+  postOrderTraversal(node.left);
+  postOrderTraversal(node.right);
+  console.log('Post-Order Traversal: ', node.value);
 };
 
+// called 'pre order' because the node is acted on before (pre) its two children
 const preOrderTraversal = (node) => {
-
+  if (!node) return;
+  
+  console.log('Pre-Order Traversal: ', node.value);
+  preOrderTraversal(node.left);
+  preOrderTraversal(node.right);
 };
+
+inOrderTraversal(tree);
+postOrderTraversal(tree);
+preOrderTraversal(tree);
