8bd678090fc7862699b5dcc1d247c2f8b5ca79eb
Patrick Miner <patrickminer318@gmail.com>
2018-04-22 19:34:49 -0700

    Completed insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,34 @@
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let valueToLeft = arr[i - 1];
+    if (valueToLeftIsSmallerOrUndefined(currentValue, valueToLeft)) {
+      continue;
+    } else {
+      arr = insertValueAndUpdateArray(currentValue, i, arr);
+    }
+  }
+
+  return arr;
+};
+
+function insertValueAndUpdateArray(currentValue, currentIndex, array) {
+  let valueToLeft = array[currentIndex - 1];
+  array[index] = valueToLeft;
+  newIndex = currentIndex - 1;
+  if (valueToLeftIsSmallerOrUndefined(currentValue, valueToLeft)){
+    array[newIndex] = currentValue;
+  } else {
+    array = insertValueAndUpdateArray(currentValue, newIndex, array);
+  }
+
+  return array;
+}
+
+function valueToLeftIsSmallerOrUndefined(currentValue, valueToLeft) {
+  return valueToLeft === undefined || left <= currentValue;
+}
+
+
+console.log(insertionSort([4, 6, 234, 6, 7, 8, 2])); // [2, 4, 6, 6, 7, 8, 234]
+console.log(insertionSort([5, 23, 1, 634, 23, 2])); // [1, 2, 5, 23, 23, 634]
