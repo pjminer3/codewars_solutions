1e267ad65d087734b97484b7cfa61604c1c18040
Patrick Miner <patrickminer318@gmail.com>
2018-04-25 23:40:28 -0700

    Found optimal solution. Top 100% in leetcode

--- findIslands.js
+++ findIslands.js
@@ -1,32 +1,42 @@
-const clearSurroundingArea = (grid, x, y) => {
-  if (grid[x - 1]) clearIsland(grid, x - 1, y);
-  if (grid[x][y - 1]) clearIsland(grid, x, y - 1);
-  if (grid[x + 1]) clearIsland(grid, x + 1, y);
-  if (grid[x][y + 1]) clearIsland(grid, x, y + 1);
-}
+// const clearSurroundingArea = (grid, x, y) => {
+//   if (grid[x - 1]) clearIsland(grid, x - 1, y);
+//   if (grid[x][y - 1]) clearIsland(grid, x, y - 1);
+//   if (grid[x + 1]) clearIsland(grid, x + 1, y);
+//   if (grid[x][y + 1]) clearIsland(grid, x, y + 1);
+// }
 
 const clearIsland = (grid, x, y) => {
+  if (x < 0 || y < 0 || x > grid.length - 1 || y > grid[0].length - 1 || grid[x][y] === "0") return;
 
-  
-  if (!grid[x][y]) return;
+  grid[x][y] = "0";
 
-  grid[x][y] = 0;
-  clearSurroundingArea(grid, x, y);
+  clearIsland(grid, x - 1, y);
+  clearIsland(grid, x, y - 1);
+  clearIsland(grid, x + 1, y);
+  clearIsland(grid, x, y + 1);
 };
 
 const numIslands = (grid) => {
-  let counter = 0;
+  let total = 0;
 
   for (let x = 0; x < grid.length; x++) {
     for (let y = 0; y < grid[0].length; y++) {
-      if (grid[x][y]) {
-        counter ++;
+      if (grid[x][y] === "1") {
+        total ++;
         clearIsland(grid, x, y);
       } 
     }
   }
 
-  return counter;
+  return total;
 };
 
+let area = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+];
+
+console.log(numIslands(area)); // 3
 
