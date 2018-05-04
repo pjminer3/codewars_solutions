7741c118c34e3b0439d1233854e5d5b7339bc45f
Patrick Miner <patrickminer318@gmail.com>
2018-05-03 23:16:50 -0700

    Completed with top 87% runtime

---
+++ LeetCode/112. Path Sum.js
@@ -0,0 +1,6 @@
+const hasPathSum = (root, sum) => {
+  if (!root) return false;
+  if (root.val === sum && !root.left && !root.right) return true;
+
+  return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+}
