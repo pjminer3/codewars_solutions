091aa8e61506c810600f9392be1897bde93bd684
Patrick Miner <patrickminer318@gmail.com>
2018-05-17 08:13:36 -0700

    Completed exercise

--- CTCI/arraysAndStrings.js
+++ CTCI/arraysAndStrings.js
@@ -29,10 +29,53 @@
 
 //1.4 One Away
 const oneAway = (str1, str2) => {
+  if (str1.length === str2.length) {
+    return changeChar(str1, str2);
+  } else if (str1.length === str2.length + 1) {
+    return addChar(str2, str1);
+  } else if (str2.length === str1.length + 1) {
+    return addChar(str1, str2);
+  }
 
+  return false;
 };
 
-console.log(oneAwy('pale', 'ple')); // true
-console.log(oneAwy('pale', 'ple')); // true
-console.log(oneAwy('pale', 'ple')); // true
-console.log(oneAwy('pale', 'ple')); // false
+function changeChar(str1, str2) {
+  let foundFirstDif = false;
+
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      if (foundFirstDif) return false;
+      foundFirstDif = true;
+    }
+  }
+
+  return true;
+};
+
+function addChar(smallStr, largeStr) {
+  let idxSmall = idxLarge = 0;
+  let foundFirstDif = false;
+
+  while (idxSmall < smallStr.length || idxLarge < largeStr.length) {
+    if (smallStr[idxSmall] !== largeStr[idxLarge]) {
+      if (foundFirstDif) return false;
+      idxLarge++;
+      foundFirstDif = true;
+    } else {
+      idxSmall++;
+      idxLarge++;
+    }
+  }
+
+  return true;
+};
+
+console.log(oneAway('pale', 'ple')); // true
+console.log(oneAway('ple', 'pale')); // true
+console.log(oneAway('pales', 'pale')); // true
+console.log(oneAway('pale', 'pales')); // true
+console.log(oneAway('pale', 'bale')); // true
+console.log(oneAway('bale', 'pale')); // true
+console.log(oneAway('pale', 'bake')); // false
+console.log(oneAway('bake', 'pale')); // false
