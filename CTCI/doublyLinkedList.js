952dfe1f0c8de0a4cf82471fe4da5ef25310d7ff
Patrick Miner <patrickminer318@gmail.com>
2018-05-18 23:43:54 -0700

    Made Doubly Linked List

---
+++ CTCI/doublyLinkedList.js
@@ -0,0 +1,22 @@
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enque(key, value) {
+
+  }
+
+  dequeue() {
+
+  }
+
+  remove(node) {
+
+  }
+
+  addToTail(key, value) {
+    
+  }
+}
